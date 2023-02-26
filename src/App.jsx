2d724f757f9c3d662e4fe28304e76c398927bf7b
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2023-02-26 20:52:25 +0100

    added some spacing and effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./components/Card";
 function App() {
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#0d192b] p-7">
+    <div className="flex items-center justify-center min-h-screen bg-[#0d192b]">
       <Card />
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,24 @@
 export default function Card() {
 
     return (
-        <div className="flex flex-col bg-[#14253d] p-7 rounded-3xl">
-            <img className="w-full rounded-xl" src="image-equilibrium.jpg" alt="nft-equilibrium" />
-            <div className="flex flex-col divide-y divide-[#2f415b] mt-2">
-                <div className="flex flex-col space-y-4 py-4">
-                    <h1 className="text-white font-outfit font-semibold text-2xl">Equilibrium #3429</h1>
+        <div className="flex flex-col bg-[#14253d] p-7 rounded-3xl space-y-4 m-7">
+
+            <div className="relative">
+                <a href="#">
+                    <img className="w-full rounded-xl hover:opacity-50" src="image-equilibrium.jpg" alt="nft-equilibrium" />
+                    <div className="flex absolute items-center justify-center w-full h-full top-0 rounded-xl bg-[#00fff7]/50 opacity-0 sm:hover:opacity-100">
+                        <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h48v48H0z" /><path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z" fill="#FFF" fill-rule="nonzero" /></g></svg>
+                    </div>
+                </a>
+            </div>
+
+            <div className="flex flex-col divide-y divide-[#2f415b]">
+                <div className="flex flex-col space-y-4 py-3">
+                    <div className="w-fit">
+                        <h1 className="text-white font-outfit font-semibold text-2xl sm:hover:text-[#00fff7] active:text-[#00fff7]">
+                            <a href="#">Equilibrium #3429</a>
+                        </h1>
+                    </div>
                     <p className="text-[#8bacda] font-outfit font-light text-lg">Our Equilibrium collection promotes balance and calm.</p>
                     <div className="flex justify-between">
                         <div className="flex items-center space-x-2">
@@ -19,15 +32,14 @@ export default function Card() {
                         </div>
                     </div>
                 </div>
-                <div className="flex items-center space-x-5 pt-4">
+                <div className="flex items-center space-x-5 pt-5">
                     <img className="w-10 border-[1px] rounded-full" src="image-avatar.png" alt="avatar-img" />
                     <div className="flex space-x-1">
                         <span className="text-[#8bacda] font-outfit font-normal text-lg">Creation of</span>
-                        <span className="text-white font-outfit font-normal text-lg">Jules Wyvern</span>
+                        <a className="text-white font-outfit font-normal text-lg sm:hover:text-[#00fff7] active:text-[#00fff7]" href="#">Jules Wyvern</a>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }

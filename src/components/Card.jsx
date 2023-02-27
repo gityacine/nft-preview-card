6cd399f73c357952cd0375c4252856612a33e773
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2023-02-27 20:29:16 +0100

    made the card responsive and added some effects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,16 @@
 export default function Card() {
 
     return (
-        <div className="flex flex-col bg-[#14253d] p-7 rounded-3xl space-y-4 m-7">
-
+        <div className="flex flex-col bg-[#14253d] p-7 rounded-3xl space-y-4 m-7 shadow-2xl">
             <div className="relative">
                 <a href="#">
-                    <img className="w-full rounded-xl hover:opacity-50" src="image-equilibrium.jpg" alt="nft-equilibrium" />
-                    <div className="flex absolute items-center justify-center w-full h-full top-0 rounded-xl bg-[#00fff7]/50 opacity-0 sm:hover:opacity-100">
+                    <img className="w-full mx-0 sm:w-80 rounded-xl" src="image-equilibrium.jpg" alt="nft-equilibrium" />
+                    <div className="flex absolute transition duration-300 items-center justify-center w-full h-full top-0 rounded-xl bg-[#00fff7]/50 opacity-0 sm:hover:opacity-100">
                         <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h48v48H0z" /><path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z" fill="#FFF" fill-rule="nonzero" /></g></svg>
                     </div>
                 </a>
             </div>
-
-            <div className="flex flex-col divide-y divide-[#2f415b]">
+            <div className="flex flex-col divide-y divide-[#2f415b] sm:w-80">
                 <div className="flex flex-col space-y-4 py-3">
                     <div className="w-fit">
                         <h1 className="text-white font-outfit font-semibold text-2xl sm:hover:text-[#00fff7] active:text-[#00fff7]">
